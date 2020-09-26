7c4d58ef67936c8ad14328e3d93fe7a391c449f8
nigel-the-great <nigel.chege.18@cnu.edu>
2020-09-26 19:09:13 -0400

    missing file

---
+++ fileio.h
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : fileio h
+// Author      : Nigel
+// Version     :
+// Copyright   : Steal this code!
+// Description : learn how to refactor files into a sensible directory structure
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../includes/constants.h"
+using namespace std;
+
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
